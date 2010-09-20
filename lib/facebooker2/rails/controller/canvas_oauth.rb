50a29f308ff64fa462229770689841404b367b5c
John Bintz <john@coswellproductions.com>
2010-09-20 11:18:08 -0400

    specs for canvas work

--- lib/facebooker2/rails/controller/canvas_oauth.rb
+++ lib/facebooker2/rails/controller/canvas_oauth.rb
@@ -16,9 +16,16 @@ module Facebooker2
             if params[:error]
               raise Facebooker2::OAuthException.new(params[:error][:message])
             else
-              redirect_to ('http://apps.facebook.com/' + Facebooker2.canvas_page_name) if params[:code]
+              # this is where you get a code for requesting an access_token to do additional OAuth requests
+              # outside of using the FB JavaScript library (see Authenticating Users in a Web Application
+              # under the Authentication docs at http://developers.facebook.com/docs/authentication/)
+              if params[:code]
+                redirect_to 'http://apps.facebook.com/' + Facebooker2.canvas_page_name
+                return false
+              else
+                raise Facebooker2::OAuthException.new('No code returned.')
+              end
             end
-            return false
           end
 
           def ensure_canvas_connected
@@ -35,6 +42,9 @@ module Facebooker2
       end
 
       module CanvasOAuthClass
+        # Make sure the application is connected to Facebook before controller actions fire.
+        # You must enabled OAuth 2.0 for Canvas (beta) and be receiving signed_request parameters
+        # from Facebook requests for this work.
         def ensure_canvas_connected_to_facebook(oauth_callback_url, *scope)
           self._facebooker_oauth_callback_url = oauth_callback_url
           self._facebooker_scope = scope
@@ -42,6 +52,7 @@ module Facebooker2
           before_filter :ensure_canvas_connected
         end
 
+        # Create an endpoint in the application to direct the OAuth authentication callback.
         def create_facebook_oauth_callback(method_name)
           self.class_eval(<<-EOT)
             def #{method_name}
